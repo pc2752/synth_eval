e4eacfc7438d4f7383f7f11c00c6f17724a6bc14
Pritish Chandna <pc2752@Pritishs-MacBook-Air.local>
2019-02-19 16:35:26 +0530

    First commit

---
+++ config/example_config_abx.js
@@ -0,0 +1,337 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Source Separation Evaluation",
+  "LoopByDefault": true,
+  "ShowFileIDs": false,
+  "ShowResults": false,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": true,
+  "BeaqleServiceURL": "/sourcesepeval/web_service/beaqleJS_Service.php",
+  "SupervisorContact": "",
+  "RandomizeTestOrder": false,
+  "MaxTestsPerRun": 45,
+  "AudioRoot": "",
+  "Testsets": [
+    //
+    {
+      "Name": "Intelligibility 1",
+      "TestID": "id1",
+      "Files": {
+        "A": "audio/ori_1.mp3",
+        "B": "audio/gl_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 2",
+      "TestID": "id2",
+      "Files": {
+        "A": "audio/npss_4.mp3",
+        "B": "audio/ori_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 3",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_3.mp3",
+        "B": "audio/gl_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 4",
+      "TestID": "id4",
+      "Files": {
+        "A": "audio/ori_2.mp3",
+        "B": "audio/npss_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 5",
+      "TestID": "id5",
+      "Files": {
+        "A": "audio/gl_1.mp3",
+        "B": "audio/npss_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 6",
+      "TestID": "id6",
+      "Files": {
+        "A": "audio/gl_2.mp3",
+        "B": "audio/gc_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 7",
+      "TestID": "id7",
+      "Files": {
+        "A": "audio/gl_3.mp3",
+        "B": "audio/ori_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 8",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_1.mp3",
+        "B": "audio/ori_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 9",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_2.mp3",
+        "B": "audio/gl_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 10",
+      "TestID": "id10",
+      "Files": {
+        "A": "audio/gc_1.mp3",
+        "B": "audio/gl_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 11",
+      "TestID": "id11",
+      "Files": {
+        "A": "audio/ori_2.mp3",
+        "B": "audio/gl_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 12",
+      "TestID": "id12",
+      "Files": {
+        "A": "audio/gl_4.mp3",
+        "B": "audio/gc_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 13",
+      "TestID": "id13",
+      "Files": {
+        "A": "audio/gl_4.mp3",
+        "B": "audio/npss_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 14",
+      "TestID": "id14",
+      "Files": {
+        "A": "audio/npss_3.mp3",
+        "B": "audio/ori_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Intelligibility 15",
+      "TestID": "id15",
+      "Files": {
+        "A": "audio/ori_4.mp3",
+        "B": "audio/gl_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },    
+    {
+      "Name": "Intelligibility 16",
+      "TestID": "id15",
+      "Files": {
+        "A": "audio/gc_3.mp3",
+        "B": "audio/gl_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },    //
+    {
+      "Name": "Audio Quality 1",
+      "TestID": "id1",
+      "Files": {
+        "A": "audio/ori_1.mp3",
+        "B": "audio/gl_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 2",
+      "TestID": "id2",
+      "Files": {
+        "A": "audio/npss_4.mp3",
+        "B": "audio/ori_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 3",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_3.mp3",
+        "B": "audio/gl_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 4",
+      "TestID": "id4",
+      "Files": {
+        "A": "audio/ori_2.mp3",
+        "B": "audio/npss_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 5",
+      "TestID": "id5",
+      "Files": {
+        "A": "audio/gl_1.mp3",
+        "B": "audio/npss_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 6",
+      "TestID": "id6",
+      "Files": {
+        "A": "audio/gl_2.mp3",
+        "B": "audio/gc_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 7",
+      "TestID": "id7",
+      "Files": {
+        "A": "audio/gl_3.mp3",
+        "B": "audio/ori_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 8",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_1.mp3",
+        "B": "audio/ori_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 9",
+      "TestID": "id3",
+      "Files": {
+        "A": "audio/npss_2.mp3",
+        "B": "audio/gl_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 10",
+      "TestID": "id10",
+      "Files": {
+        "A": "audio/gc_1.mp3",
+        "B": "audio/gl_1.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 11",
+      "TestID": "id11",
+      "Files": {
+        "A": "audio/ori_2.mp3",
+        "B": "audio/gl_2.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 12",
+      "TestID": "id12",
+      "Files": {
+        "A": "audio/gl_4.mp3",
+        "B": "audio/gc_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 13",
+      "TestID": "id13",
+      "Files": {
+        "A": "audio/gl_4.mp3",
+        "B": "audio/npss_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 14",
+      "TestID": "id14",
+      "Files": {
+        "A": "audio/npss_3.mp3",
+        "B": "audio/ori_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+    {
+      "Name": "Audio Quality 15",
+      "TestID": "id15",
+      "Files": {
+        "A": "audio/ori_4.mp3",
+        "B": "audio/gl_4.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+        {
+      "Name": "Audio Quality 16",
+      "TestID": "id16",
+      "Files": {
+        "A": "audio/gc_3.mp3",
+        "B": "audio/gl_3.mp3",
+        "X": " ",
+        "Y": " ",
+      }
+    },
+  ]
+}
---
+++ config/key.py
@@ -0,0 +1,5 @@
+key_set = [{"type":'o_g', 'o':0, 'g':1, "clip":1}, {"type":'o_n', 'n':0, 'o':1, "clip":4}, {"type":'g_n', 'n':0, 'g':1, "clip":3},
+{"type":'o_n', 'o':0, 'n':1, "clip":2}, {"type":'g_n', 'g':0, 'n':1, "clip":1}, {"type":'g_c', 'g':0, 'c':1, "clip":2},  {"type":'o_g', 'g':0, 'o':1, "clip":3},  
+  {"type":'o_n', 'n':0, 'o':1, "clip":1}, {"type":'g_n', 'n':0, 'g':1, "clip":2},{"type":'g_c', 'c':0, 'g':1, "clip":1}, {"type":'o_g', 'o':0, 'g':1, "clip":2},    
+{"type":'g_c', 'g':0, 'c':1, "clip":4}, {"type":'g_n', 'g':0, 'n':1, "clip":4}, {"type":'o_n', 'n':0, 'o':1, "clip":3}, {"type":'o_g', 'o':0, 'g':1, "clip":4},
+{"type":'g_c', 'c':0, 'g':1, "clip":3}]
